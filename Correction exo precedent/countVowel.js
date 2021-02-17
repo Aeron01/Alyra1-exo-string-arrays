1366dce38aa0a4fab2c47e26c7ecc3a93fc0c372
Aeron01 <one.soulreaper@gmail.com>
2021-02-17 15:22:41 +0100

    update 1 file

--- Correction exo precedent/countVowel.js
+++ Correction exo precedent/countVowel.js
@@ -1,16 +1,70 @@
 // Programme de comptage de tous les voyelles dans un texte
 
-let txt = 'Je suis le ténébreux, - le veuf, - l\'inconsolé,' + ' Le prince d\'Aquitaine à la tour abolie :' + ' Ma seule étoile est morte, - et mon luth constellé' + ' Porte le soleil noir de la Mélancolie.' + ' Dans la nuit du tombeau, toi qui m\'as consolé,' + ' Rends - moi le Pausilippe et la mer d\'Italie,' + ' La fleur qui plaisait tant à mon cœur désolé,' + ' Et la treille où le pampre à la rose s\'allie.' + ' Suis - je Amour ou Phébus ? ...Lusignan ou Biron ?' + ' Mon front est rouge encor du baiser de la reine;' + ' J\'ai rêvé dans la grotte où nage la sirène...' + ' Et j\'ai deux fois vainqueur traversé l\'Achéron;' + ' Modulant tour à tour sur la lyre d\'Orphée' + ' Les soupirs de la sainte et les cris de la fée.'
-let nbe = 0 // initialisation du compteur des voyelles
+let txt = "Je suis le ténébreux, - le veuf, - l'inconsolé,\n\
+Le prince d'Aquitaine à la tour abolie :\n\
+Ma seule étoile est morte, - et mon luth constellé\n\
+Porte le soleil noir de la Mélancolie.\n\
+Dans la nuit du tombeau, toi qui m'as consolé,\n\
+Rends - moi le Pausilippe et la mer d'Italie,\n\
+La fleur qui plaisait tant à mon cœur désolé,\n\
+Et la treille où le pampre à la rose s'allie.\n\
+Suis - je Amour ou Phébus ? ...Lusignan ou Biron ?\n\
+Mon front est rouge encor du baiser de la reine;\n\
+J'ai rêvé dans la grotte où nage la sirène...\n\
+Et j'ai deux fois vainqueur traversé l'Achéron;\n\
+Modulant tour à tour sur la lyre d'Orphée\n\
+Les soupirs de la sainte et les cris de la fée."
+let nbvyl = 0 // initialisation du compteur des voyelles
+let nbA = 0
+let nbE = 0
+let nbI = 0
+let nbO = 0
+let nbU = 0
+let nbY = 0
 
 console.log(`${txt}\n`) // affichage du texte
 txtlc = txt.toLowerCase() // transformation de toutes les majuscules dans le texte en minuscule
 
 // boucle d'incrémentation pour traiter tous les caractères du texte
-for (let nbe0 = 0; nbe0 < txtlc.length; ++nbe0) {
+for (let ivyl = 0; ivyl < txtlc.length; ++ivyl) {
   // comparateur des caractéres par rapport à ceux rechercher et comptage des voyelles par incrémentation
-  if (txtlc[nbe0] === 'a' || txtlc[nbe0] === 'e' || txtlc[nbe0] === 'i' || txtlc[nbe0] === 'o' || txtlc[nbe0] === 'u' || txtlc[nbe0] === 'y')
-    nbe++
+  if (
+    txtlc[ivyl] === 'a' ||
+    txtlc[ivyl] === 'à' ||
+    txtlc[ivyl] === 'ä' ||
+    txtlc[ivyl] === 'â') {
+    ++nbA
+  } else if (txtlc[ivyl] === 'e' ||
+    txtlc[ivyl] === 'é' ||
+    txtlc[ivyl] === 'è' ||
+    txtlc[ivyl] === 'ê') {
+    ++nbE
+  } else if (txtlc[ivyl] === 'i' ||
+    txtlc[ivyl] === 'ï' ||
+    txtlc[ivyl] === 'î') {
+    ++nbI
+  } else if (txtlc[ivyl] === 'o' ||
+    txtlc[ivyl] === 'ô' ||
+    txtlc[ivyl] === 'ö') {
+    ++nbO
+  } else if (txtlc[ivyl] === 'u' ||
+    txtlc[ivyl] === 'ù' ||
+    txtlc[ivyl] === 'ü' ||
+    txtlc[ivyl] === 'û') {
+    ++nbU
+  } else if (txtlc[ivyl] === 'y' ||
+    txtlc[ivyl] === 'ÿ' ||
+    txtlc[ivyl] === 'ŷ') {
+    ++nbY
+  }
+  ++nbvyl
 }
 
-console.log(`Il y a ${nbe} voyelle(s) dans ce texte.`) // affichage du résultat
+console.log(`Il y a ${nbvyl} voyelle(s) dans ce texte.`) // affichage du résultat
+
+console.log(`nb E: ${nbE}`)
+console.log(`nb A: ${nbA}`)
+console.log(`nb I: ${nbI}`)
+console.log(`nb O: ${nbO}`)
+console.log(`nb U: ${nbU}`)
+console.log(`nb Y: ${nbY}`)
