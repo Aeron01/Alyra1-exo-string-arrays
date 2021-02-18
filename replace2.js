a8438b0853018457639cdcfb57904faa242c03d6
Aeron01 <one.soulreaper@gmail.com>
2021-02-18 10:01:09 +0100

    1 file update (is working fine)

--- replace2.js
+++ replace2.js
@@ -1,17 +1,17 @@
-// cela marche mais pas comme je le voudrais. a revoir
+// Il est en vie :)
 
 let txt = "In cryptography, Alice and Bob are fictional characters commonly used as placeholders in discussions about cryptographic protocols or systems.\n\
 As the use of Alice and Bob became more widespread, additional characters were added, sometimes each with a particular meaning.\n\
 The most common characters are Alice and Bob.Eve, Mallory, and Trent are also common names.Bonjour, je suis Sofiane."
-// let name = 'Alice'
+let name = 'Bob'
 let replname = 'Christian'
 // D'abord faire une recherche avec la commande .indexof pour voir si le nom est présent
-let indx = txt.indexOf('Alice')
+let indx = txt.indexOf(name)
 console.log(`${txt}\n`)
-// if (indx > -1) {
-// si le nom est présent, procéder au remplacement grace a la commande .replace
-let newtxt = txt.replace(/Alice/gi, replname)
-console.log(newtxt)
-// } else {
-//  console.log(`le nom ${name} n'est pas dans la phrase`)
-// }
+if (indx > -1) {
+  // si le nom est présent, procéder au remplacement grace a la commande .replaceAll sur toutes les ligne
+  let newtxt = txt.replaceAll(name, replname)
+  console.log(newtxt)
+} else {
+  console.log(`le nom ${name} n'est pas dans la phrase`)
+}
